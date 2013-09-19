5b8050ab1b35c5aab56ad4410b02140239963d85
James Cox <james@imaj.es>
2013-09-19 11:49:37 -0400

    option to replace helvetica also. it's an amazing font, but arial has spoilt it for me.

--- allvetica.js
+++ allvetica.js
@@ -4,24 +4,25 @@ chrome.extension.sendRequest(
     { data: 'options' },
     function (response) {
         var options = response.options;
-        
+
         var fonts = [];
+        if (options.replaceHelvetica)   fonts.push("Helvetica");
         if (options.replaceArial)       fonts.push("Arial");
         if (options.replaceComicSans)   fonts.push("Comic\\s*Sans\\s*(MS|2010)?");
         if (options.replaceMarkerFelt)  fonts.push("Marker\\s*Felt");
         if (options.replacePapyrus)     fonts.push("Papyrus");
-        
+
         var replacement = options.replacement;
-        
+
         if (!replacement && options.replacementCustomValid) {
             replacement = options.replacementCustom;
         }
-        
+
         if (replacement && fonts.length) {
             fixInvalidStyles(fonts, replacement);
             replaceFont(fonts, replacement);
         }
-        
+
         if (options.optimizeLegibility)
             jQuery('body').css('text-rendering', 'optimizeLegibility');
     }
@@ -31,19 +32,19 @@ chrome.extension.sendRequest(
 // Attempt to fix and reinject invalid CSS for more robust font replacement
 function fixInvalidStyles (fonts, replacement) {
     var allStyles = '';
-    
+
     jQuery('head style').each( function (i, e) {
         var style = jQuery(this).html();
         var invalidContent = new RegExp( '<!--|-->|<\\w[^>]*>', 'g' );
-        
+
         if (style.match( invalidContent )) {
             var toReplace = new RegExp("[\\'\\\"\\s]*(" + fonts.join('|') + ")[\\'\\\"\\s]*(?=,|)", "gi");
-        
+
             style = style.replace( invalidContent, '' );
             allStyles += style.replace( toReplace, "'" + replacement + "'");
         }
     } );
-    
+
     jQuery('head').append('<style type="text/css">' + allStyles + '</style>');
 }
 
@@ -57,15 +58,15 @@ function replaceFont (fonts, replacement) {
     if (document.styleSheets) {
         for (var i = 0; i < document.styleSheets.length; i++) {
             var styleSheet = document.styleSheets[i];
-            
+
             if (styleSheet.cssRules) {
                 for (var j = 0; j < styleSheet.cssRules.length; j++) {
                     var cssRule = styleSheet.cssRules[j];
-                    
+
                     // Replace in font-family attribute
                     if (cssRule.style.fontFamily.match(toReplace))
                         cssRule.style.fontFamily = cssRule.style.fontFamily.replace(toReplace, replacement);
-        
+
                     // Replace in font attribute
                     if (cssRule.style.font.match(toReplace))
                         cssRule.style.font = cssRule.style.font.replace(toReplace, replacement);
@@ -73,13 +74,13 @@ function replaceFont (fonts, replacement) {
             }
         }
     }
-    
+
     // Adjust inline styles
     jQuery('body').find('*').each(
         function (i) {
             if (jQuery(this).css('font-family').match(toReplace))
                 jQuery(this).css('font-family', jQuery(this).css('font-family').replace(toReplace, replacement));
-            
+
             if (jQuery(this).css('font').match(toReplace))
                 jQuery(this).css('font', jQuery(this).css('font').replace(toReplace, replacement));
         }
@@ -92,5 +93,5 @@ function replaceFont (fonts, replacement) {
                 jQuery(this).attr('face', jQuery(this).attr('face').replace(toReplace, replacement));
         }
     );
-    
+
 }
--- options.js
+++ options.js
@@ -13,28 +13,31 @@ function initOptions () {
             // jQuery(this).remove();
         }
     } );
-    
+
     var options = JSON.parse( localStorage.allvetica );
-    
+
     jQuery('#replacement option').filter('[value="' + options.replacement + '"]').attr('selected', true);
+
+    jQuery('#replace_helvetica')
+        .attr('checked', options.replaceHelvetica);
 
     jQuery('#replace_arial')
         .attr('checked', options.replaceArial);
-        
+
     jQuery('#replace_comic_sans')
         .attr('checked', options.replaceComicSans);
-        
+
     jQuery('#replace_marker_felt')
         .attr('checked', options.replaceMarkerFelt);
-        
+
     jQuery('#replace_papyrus')
         .attr('checked', options.replacePapyrus);
-    
+
     jQuery('#optimize_legibility')
         .attr('checked', options.optimizeLegibility);
 
     jQuery('#replacement_custom').val(options.replacementCustom);
-    
+
     if (jQuery('#replacement option:selected').hasClass('custom')) {
         jQuery('#preview').show();
         jQuery('#replacement-advanced').show();
@@ -47,11 +50,11 @@ function initOptions () {
 
 function saveOptions () {
     var options = {};
-    
+
     // custom replacement font
     var customReplacementFont = jQuery('#replacement_custom').val();
     var customFontInstalled = isFontAvailable( customReplacementFont );
-    
+
     if (jQuery('#replacement option:selected').hasClass('custom')) {
         jQuery('#replacement-advanced').slideDown('fast');
         checkFont(true);
@@ -60,11 +63,11 @@ function saveOptions () {
         jQuery('#preview').slideUp('fast');
         jQuery('#warnings').slideUp('fast');
     }
-    
+
     if (customReplacementFont && customFontInstalled) {
         options.replacementCustom = customReplacementFont;
         options.replacementCustomValid = true;
-        
+
     } else if (customReplacementFont) {
         // Invalid
         var previous = JSON.parse( localStorage.allvetica );
@@ -76,9 +79,12 @@ function saveOptions () {
         options.replacementCustomValid = false;
     }
 
+    options.replaceHelvetica
+        = jQuery('#replace_helvetica:checked').val() != null;
+
     options.replaceArial
         = jQuery('#replace_arial:checked').val() != null;
-    
+
     options.replaceComicSans
         = jQuery('#replace_comic_sans:checked').val() != null;
 
@@ -87,21 +93,21 @@ function saveOptions () {
 
     options.replacePapyrus
         = jQuery('#replace_papyrus:checked').val() != null;
-    
+
     options.replacement
         = jQuery('#replacement option:selected').val();
-    
+
     options.optimizeLegibility
         = jQuery('#optimize_legibility:checked').val() != null;
-    
+
     localStorage.allvetica = JSON.stringify( options );
 }
 
 function checkFont (saving) {
     var fontName = jQuery('#replacement_custom').val();
-    
+
     var customFontInstalled = (fontName) ? isFontAvailable( fontName ) : false;
-    
+
     if (customFontInstalled) {
         jQuery('#replacement_custom').removeClass('notfound');
         if (saving) {
@@ -109,7 +115,7 @@ function checkFont (saving) {
             jQuery('#custom-font-sample').css('font-family', fontName);
             jQuery('#preview').slideDown('fast');
         }
-        
+
     } else {
         jQuery('#replacement_custom').addClass('notfound');
         if (saving) {
@@ -125,18 +131,18 @@ function checkFont (saving) {
 function isFontAvailable (font) {
     var testString  = '~iomwIOMW';
     var containerId = 'is-font-available-container';
-    
+
     var fontArray = font instanceof Array;
-    
+
     if (!fontArray) {
         font = [ font ];
     }
-    
+
     var fontAvailability = [];
-    
+
     var containerSel = '#' + containerId;
     var spanSel      = containerSel + ' span';
-        
+
     var familySansSerif = 'sans-serif';
     var familyMonospace = 'monospace, monospace';
     // Why monospace twice? It's a bug in the Mozilla and Webkit rendering engines:
@@ -146,7 +152,7 @@ function isFontAvailable (font) {
     $('body').append('<div id="' + containerId + '"></div>');
     $(containerSel).append('<span></span>');
     $(spanSel).append(document.createTextNode(testString));
-    
+
     // CSS:
     $(containerSel).css('visibility', 'hidden');
     $(containerSel).css('position', 'absolute');
@@ -154,27 +160,27 @@ function isFontAvailable (font) {
     $(containerSel).css('top', '0');
     $(containerSel).css('font-weight', 'bold');
     $(containerSel).css('font-size', '200px !important');
-    
+
     jQuery.each( font, function (i, v) {
         $(spanSel).css('font-family', v + ',' + familyMonospace );
         var monospaceFallbackWidth = $(spanSel).width();
         var monospaceFallbackHeight = $(spanSel).height();
-        
+
         $(spanSel).css('font-family', v + ',' + familySansSerif );
         var sansSerifFallbackWidth = $(spanSel).width();
         var sansSerifFallbackHeight = $(spanSel).height();
-        
+
         fontAvailability[i] = true
             && monospaceFallbackWidth == sansSerifFallbackWidth
             && monospaceFallbackHeight == sansSerifFallbackHeight;
     } );
-    
+
     $(containerSel).remove();
-    
+
     if (!fontArray && fontAvailability.length == 1) {
         fontAvailability = fontAvailability[0];
     }
-    
+
     return fontAvailability;
 }
 
